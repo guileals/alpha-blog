28e732c63180a52788ca1e7cafea34dc0b9e9623
Guilherme Santos <g.santos@outlook.com>
2022-06-02 17:17:25 -0300

    Code after DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
     
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+
     def show
-        # byebug
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -12,10 +12,27 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+
+    def edit
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path, status: :see_other # Rails 7 fix!!!
+    end
+    
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully!"
+            redirect_to @article
+        else
+            render 'edit', status: :unprocessable_entity
+        end
+    end
      
     def create 
         # render plain: params[:article]
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was created successfully!"
             redirect_to @article
@@ -23,5 +40,15 @@ class ArticlesController < ApplicationController
             render 'new', status: :unprocessable_entity
         end
     end 
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
 
 end
