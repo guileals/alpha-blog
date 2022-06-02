2b3f5d472bfa366e58a8cc6293226781920625bf
Guilherme Santos <g.santos@outlook.com>
2022-06-02 14:43:14 -0300

    Flash messages OK

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,27 @@
 class ArticlesController < ApplicationController
     
     def show
-        byebug
+        # byebug
         @article = Article.find(params[:id])
     end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+     
+    def create 
+        # render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created successfully!"
+            redirect_to @article
+        else
+            render 'new', status: :unprocessable_entity
+        end
+    end 
 
 end
