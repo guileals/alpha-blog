b478c0324e77b6f021b13bc572c8d94b59484d08
Guilherme Santos <g.santos@outlook.com>
2022-06-13 17:08:51 -0300

    Styling homepage

---
+++ app/assets/config/importmap.rb
@@ -0,0 +1,3 @@
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.5/lib/index.js"
