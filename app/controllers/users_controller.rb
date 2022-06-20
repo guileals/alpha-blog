d31b39451352603dbc62eaa55847b408abb5f923
Guilherme Santos <g.santos@outlook.com>
2022-06-20 18:33:20 -0300

    Sign up OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,25 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Usuário criado com sucesso! Bem vindo, #{@user.username}!}"
+            redirect_to articles_path
+        else
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
